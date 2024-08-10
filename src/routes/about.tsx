33e9c82ce49aee73cd00fa27a03869d7a2e3ad06
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-08-10 14:50:04 -0700

    add demo fetch

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
+const getHello = () => fetch("/api/hello").then((res) => res.json());
+
 export function About() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getHello().then((res) => setData(res));
+  }, []);
+
   return (
     <div>
       <h1>About Page is Here</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>loading data...</p>
+      )}
     </div>
   );
 }
