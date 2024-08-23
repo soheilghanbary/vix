2a87537f51593b6f471fa78dd7678fd97ec1d7b2
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-08-23 05:06:45 -0700

    update more

--- server/index.ts
+++ server/index.ts
@@ -1,36 +1,36 @@
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
+import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
 
 const app = new Hono();
 
 // middlewares
-app.use("*", logger());
+app.use('*', logger());
 app.use(
-  "*",
+  '*',
   cors({
     origin: (origin) => origin,
-    allowHeaders: ["Content-Type"],
+    allowHeaders: ['Content-Type'],
     credentials: true,
-  })
+  }),
 );
 
 // routes
 const apiRoutes = app
-  .basePath("/api")
-  .get("/hello", (c) => c.json({ msg: "Hello from Hono🔥!" }));
+  .basePath('/api')
+  .get('/hello', (c) => c.json({ msg: 'Hello from Hono🔥!' }));
 
 // static files
-app.get("*", serveStatic({ root: "./dist" }));
-app.get("*", serveStatic({ path: "./dist/index.html" }));
+app.get('*', serveStatic({ root: './dist' }));
+app.get('*', serveStatic({ path: './dist/index.html' }));
 
 serve({
   fetch: app.fetch,
   port: Number(process.env.PORT) || 3000,
 });
 
-console.log("Server is running on port http://localhost:3000");
+console.log('Server Running on Port 3000 🚀');
 
 export type ApiRoutes = typeof apiRoutes;
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     TanStackRouterVite({
       generatedRouteTree: './src/types/routeTree.gen.ts',
     }),
-    react(),
   ],
   server: {
     proxy: {
